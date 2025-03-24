eba13e75a4e0aa8ae00dfbbbd1acb40aade9bd79
pepijn-devries <peppi2@gmail.com>
2025-03-24 10:13:25 +0100

    Some text edits and improved test coverage

---
+++ tests/testthat/test-plothourglass.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+test_that("Hourglass plot looks OK", {
+  vdiffr::expect_doppelganger("basic hourglass", {
+
+    data(bats)
+    
+    bats_sub <- subset(bats, format(RECDATETIME, "%Y") == "2018")
+    
+    lon <- attr(bats, "monitoring")$longitude[1]
+    lat <- attr(bats, "monitoring")$latitude[1]
+    
+    ggplot(bats_sub, aes(x = RECDATETIME, col = SPECDESCSCI)) +
+      annotate_daylight(lon, lat) +
+      annotate_daylight(lon, lat, c("dusk", "dawn")) +
+      geom_hourglass() +
+      labs(x = "Date", y = "Time of day", col = "Species")
+  })
+})
